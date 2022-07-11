0621a621a3e1ad64eae60cf5fecc1ec5f9ee6562
周悦秋 <zhouyueqiu@easycorp.ltd>
2022-07-11 13:42:02 +0800

    * Add the test environment configuration.

---
+++ frontend/tmp/log/php.20220711.log.php
@@ -0,0 +1,152 @@
+<?php
+ die();
+?>
+
+10:21:01 Uncaught Error: Call to a member function query() on null in /apps/qucheng/lib/base/dao/dao.class.php:694
+Stack trace:
+#0 /apps/qucheng/lib/base/dao/dao.class.php(797): baseDAO->query('SELECT * FROM `...')
+#1 /apps/qucheng/module/setting/model.php(25): baseDAO->fetch('value')
+#2 /apps/qucheng/module/common/model.php(64): settingModel->getItem('owner=system&mo...')
+#3 /apps/qucheng/module/common/model.php(28): commonModel->sendHeader()
+#4 /apps/qucheng/framework/base/router.class.php(1340): commonModel->__construct()
+#5 /apps/qucheng/www/upgrade-cli.php(29): baseRouter->loadCommon()
+#6 {main}
+  thrown in /apps/qucheng/lib/base/dao/dao.class.php on line 694 when visiting 
+
+10:22:43 Uncaught Error: Call to a member function query() on null in /apps/qucheng/lib/base/dao/dao.class.php:694
+Stack trace:
+#0 /apps/qucheng/lib/base/dao/dao.class.php(797): baseDAO->query('SELECT * FROM `...')
+#1 /apps/qucheng/module/setting/model.php(25): baseDAO->fetch('value')
+#2 /apps/qucheng/module/common/model.php(64): settingModel->getItem('owner=system&mo...')
+#3 /apps/qucheng/module/common/model.php(28): commonModel->sendHeader()
+#4 /apps/qucheng/framework/base/router.class.php(1340): commonModel->__construct()
+#5 /apps/qucheng/www/upgrade-cli.php(29): baseRouter->loadCommon()
+#6 {main}
+  thrown in /apps/qucheng/lib/base/dao/dao.class.php on line 694 when visiting 
+
+10:23:37 Uncaught Error: Call to a member function query() on null in /apps/qucheng/lib/base/dao/dao.class.php:694
+Stack trace:
+#0 /apps/qucheng/lib/base/dao/dao.class.php(797): baseDAO->query('SELECT * FROM `...')
+#1 /apps/qucheng/module/setting/model.php(25): baseDAO->fetch('value')
+#2 /apps/qucheng/module/common/model.php(64): settingModel->getItem('owner=system&mo...')
+#3 /apps/qucheng/module/common/model.php(28): commonModel->sendHeader()
+#4 /apps/qucheng/framework/base/router.class.php(1340): commonModel->__construct()
+#5 /apps/qucheng/www/upgrade-cli.php(29): baseRouter->loadCommon()
+#6 {main}
+  thrown in /apps/qucheng/lib/base/dao/dao.class.php on line 694 when visiting 
+
+10:24:04 Uncaught Error: Call to a member function query() on null in /apps/qucheng/lib/base/dao/dao.class.php:694
+Stack trace:
+#0 /apps/qucheng/lib/base/dao/dao.class.php(797): baseDAO->query('SELECT * FROM `...')
+#1 /apps/qucheng/module/setting/model.php(25): baseDAO->fetch('value')
+#2 /apps/qucheng/module/common/model.php(64): settingModel->getItem('owner=system&mo...')
+#3 /apps/qucheng/module/common/model.php(28): commonModel->sendHeader()
+#4 /apps/qucheng/framework/base/router.class.php(1340): commonModel->__construct()
+#5 /apps/qucheng/www/upgrade-cli.php(29): baseRouter->loadCommon()
+#6 {main}
+  thrown in /apps/qucheng/lib/base/dao/dao.class.php on line 694 when visiting 
+
+10:24:08 Uncaught Error: Call to a member function query() on null in /apps/qucheng/lib/base/dao/dao.class.php:694
+Stack trace:
+#0 /apps/qucheng/lib/base/dao/dao.class.php(797): baseDAO->query('SELECT * FROM `...')
+#1 /apps/qucheng/module/setting/model.php(25): baseDAO->fetch('value')
+#2 /apps/qucheng/module/common/model.php(64): settingModel->getItem('owner=system&mo...')
+#3 /apps/qucheng/module/common/model.php(28): commonModel->sendHeader()
+#4 /apps/qucheng/framework/base/router.class.php(1340): commonModel->__construct()
+#5 /apps/qucheng/www/upgrade-cli.php(29): baseRouter->loadCommon()
+#6 {main}
+  thrown in /apps/qucheng/lib/base/dao/dao.class.php on line 694 when visiting 
+
+10:24:18 Uncaught Error: Call to a member function query() on null in /apps/qucheng/lib/base/dao/dao.class.php:694
+Stack trace:
+#0 /apps/qucheng/lib/base/dao/dao.class.php(797): baseDAO->query('SELECT * FROM `...')
+#1 /apps/qucheng/module/setting/model.php(25): baseDAO->fetch('value')
+#2 /apps/qucheng/module/common/model.php(64): settingModel->getItem('owner=system&mo...')
+#3 /apps/qucheng/module/common/model.php(28): commonModel->sendHeader()
+#4 /apps/qucheng/framework/base/router.class.php(1340): commonModel->__construct()
+#5 /apps/qucheng/www/upgrade-cli.php(29): baseRouter->loadCommon()
+#6 {main}
+  thrown in /apps/qucheng/lib/base/dao/dao.class.php on line 694 when visiting 
+
+10:35:02 Uncaught Error: Call to a member function query() on null in /apps/qucheng/lib/base/dao/dao.class.php:694
+Stack trace:
+#0 /apps/qucheng/lib/base/dao/dao.class.php(797): baseDAO->query('SELECT * FROM `...')
+#1 /apps/qucheng/module/setting/model.php(25): baseDAO->fetch('value')
+#2 /apps/qucheng/module/common/model.php(64): settingModel->getItem('owner=system&mo...')
+#3 /apps/qucheng/module/common/model.php(28): commonModel->sendHeader()
+#4 /apps/qucheng/framework/base/router.class.php(1340): commonModel->__construct()
+#5 /apps/qucheng/www/upgrade-cli.php(29): baseRouter->loadCommon()
+#6 {main}
+  thrown in /apps/qucheng/lib/base/dao/dao.class.php on line 694 when visiting 
+
+10:37:14 Uncaught Error: Call to a member function query() on null in /apps/qucheng/lib/base/dao/dao.class.php:694
+Stack trace:
+#0 /apps/qucheng/lib/base/dao/dao.class.php(797): baseDAO->query('SELECT * FROM `...')
+#1 /apps/qucheng/module/setting/model.php(25): baseDAO->fetch('value')
+#2 /apps/qucheng/module/common/model.php(64): settingModel->getItem('owner=system&mo...')
+#3 /apps/qucheng/module/common/model.php(28): commonModel->sendHeader()
+#4 /apps/qucheng/framework/base/router.class.php(1340): commonModel->__construct()
+#5 /apps/qucheng/www/upgrade-cli.php(29): baseRouter->loadCommon()
+#6 {main}
+  thrown in /apps/qucheng/lib/base/dao/dao.class.php on line 694 when visiting 
+
+10:43:27 Uncaught Error: Call to a member function query() on null in /apps/qucheng/lib/base/dao/dao.class.php:694
+Stack trace:
+#0 /apps/qucheng/lib/base/dao/dao.class.php(797): baseDAO->query('SELECT * FROM `...')
+#1 /apps/qucheng/module/setting/model.php(25): baseDAO->fetch('value')
+#2 /apps/qucheng/module/common/model.php(64): settingModel->getItem('owner=system&mo...')
+#3 /apps/qucheng/module/common/model.php(28): commonModel->sendHeader()
+#4 /apps/qucheng/framework/base/router.class.php(1340): commonModel->__construct()
+#5 /apps/qucheng/www/upgrade-cli.php(29): baseRouter->loadCommon()
+#6 {main}
+  thrown in /apps/qucheng/lib/base/dao/dao.class.php on line 694 when visiting 
+
+10:44:30 Uncaught Error: Call to a member function query() on null in /apps/qucheng/lib/base/dao/dao.class.php:694
+Stack trace:
+#0 /apps/qucheng/lib/base/dao/dao.class.php(797): baseDAO->query('SELECT * FROM `...')
+#1 /apps/qucheng/module/setting/model.php(25): baseDAO->fetch('value')
+#2 /apps/qucheng/module/common/model.php(64): settingModel->getItem('owner=system&mo...')
+#3 /apps/qucheng/module/common/model.php(28): commonModel->sendHeader()
+#4 /apps/qucheng/framework/base/router.class.php(1340): commonModel->__construct()
+#5 /apps/qucheng/www/upgrade-cli.php(29): baseRouter->loadCommon()
+#6 {main}
+  thrown in /apps/qucheng/lib/base/dao/dao.class.php on line 694 when visiting 
+
+10:48:19 Uncaught Error: Call to a member function query() on null in /apps/qucheng/lib/base/dao/dao.class.php:694
+Stack trace:
+#0 /apps/qucheng/lib/base/dao/dao.class.php(797): baseDAO->query('SELECT * FROM `...')
+#1 /apps/qucheng/module/setting/model.php(25): baseDAO->fetch('value')
+#2 /apps/qucheng/module/common/model.php(64): settingModel->getItem('owner=system&mo...')
+#3 /apps/qucheng/module/common/model.php(28): commonModel->sendHeader()
+#4 /apps/qucheng/framework/base/router.class.php(1340): commonModel->__construct()
+#5 /apps/qucheng/www/upgrade-cli.php(29): baseRouter->loadCommon()
+#6 {main}
+  thrown in /apps/qucheng/lib/base/dao/dao.class.php on line 694 when visiting 
+
+10:51:03 Uncaught Error: Call to a member function query() on null in /apps/qucheng/lib/base/dao/dao.class.php:694
+Stack trace:
+#0 /apps/qucheng/lib/base/dao/dao.class.php(797): baseDAO->query('SELECT * FROM `...')
+#1 /apps/qucheng/module/setting/model.php(25): baseDAO->fetch('value')
+#2 /apps/qucheng/module/common/model.php(64): settingModel->getItem('owner=system&mo...')
+#3 /apps/qucheng/module/common/model.php(28): commonModel->sendHeader()
+#4 /apps/qucheng/framework/base/router.class.php(1340): commonModel->__construct()
+#5 /apps/qucheng/www/upgrade-cli.php(29): baseRouter->loadCommon()
+#6 {main}
+  thrown in /apps/qucheng/lib/base/dao/dao.class.php on line 694 when visiting 
+
+10:59:39 Uncaught Error: Call to a member function query() on null in /apps/qucheng/lib/base/dao/dao.class.php:694
+Stack trace:
+#0 /apps/qucheng/lib/base/dao/dao.class.php(797): baseDAO->query('SELECT * FROM `...')
+#1 /apps/qucheng/module/setting/model.php(25): baseDAO->fetch('value')
+#2 /apps/qucheng/module/common/model.php(64): settingModel->getItem('owner=system&mo...')
+#3 /apps/qucheng/module/common/model.php(28): commonModel->sendHeader()
+#4 /apps/qucheng/framework/base/router.class.php(1340): commonModel->__construct()
+#5 /apps/qucheng/www/upgrade-cli.php(29): baseRouter->loadCommon()
+#6 {main}
+  thrown in /apps/qucheng/lib/base/dao/dao.class.php on line 694 when visiting 
+
+11:14:52 ERROR: the module misc has no checkupdate method in framework/base/router.class.php on line 1992, last called by www/index.php on line 88 through function loadModule.
+ in /apps/qucheng/framework/base/router.class.php on line 2651 when visiting misc-checkUpdate-
+
+11:14:52 ERROR: the module misc has no checkupdate method in framework/base/router.class.php on line 1992, last called by www/index.php on line 88 through function loadModule.
+ in /apps/qucheng/framework/base/router.class.php on line 2651 when visiting misc-checkUpdate-
