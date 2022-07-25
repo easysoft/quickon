c2df73d8c12179bfe44a82c5ed372e357773e0fe
wangjianhua <wangjianhua@easycorp.ltd>
2022-07-25 09:41:00 +0000

    * Redefine uninstall success code to 200 or 404.

--- frontend/module/instance/control.php
+++ frontend/module/instance/control.php
@@ -242,7 +242,7 @@ class instance extends control
 
         $result = $this->instance->uninstall($instance);
         $this->action->create('instance', $instance->id, 'uninstall', '', json_encode(array('result' => $result, 'app' => array('alias' => $instance->appName, 'app_version' => $instance->version))));
-        if($result->code == 200) return $this->send(array('result' => 'success', 'message' => zget($this->lang->instance->notices, 'uninstallSuccess'), 'locate' => $this->createLink('space', 'browse')));
+        if($result->code == 200 || $result->code == 404) return $this->send(array('result' => 'success', 'message' => zget($this->lang->instance->notices, 'uninstallSuccess'), 'locate' => $this->createLink('space', 'browse')));
 
         return $this->send(array('result' => 'fail', 'message' => zget($this->lang->instance->notices, 'uninstallFail')));
     }
