ae8a79f143887ae4d09390071970bfbd89628097
zhouxin <zhouxin@easycorp.ltd>
2022-08-01 07:10:46 +0000

    * Add update1.2.0.sql.

--- frontend/module/upgrade/lang/version.php
+++ frontend/module/upgrade/lang/version.php
@@ -4,3 +4,4 @@ $lang->upgrade->fromVersions['1_0']   = '1.0';
 $lang->upgrade->fromVersions['1_0_3'] = '1.0.3';
 $lang->upgrade->fromVersions['1_1_1'] = '1.1.1';
 $lang->upgrade->fromVersions['1_1_2'] = '1.1.2';
+$lang->upgrade->fromVersions['1_2_0'] = '1.2.0';
