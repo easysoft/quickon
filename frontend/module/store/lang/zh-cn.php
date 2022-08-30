cee25592d0fc94bfa7f38ea16a64f302ecb0906c
wangjianhua <wangjianhua@easycorp.ltd>
2022-08-30 15:58:03 +0800

    * Change button titles.

--- frontend/module/store/lang/zh-cn.php
+++ frontend/module/store/lang/zh-cn.php
@@ -9,14 +9,14 @@ $lang->store->customInstall    = '自定义安装';
 $lang->store->searchApp        = '查找应用';
 $lang->store->testChannel      = '测试版';
 $lang->store->stableChannel    = '稳定版';
-$lang->store->appBaseInfo      = '基本信息';
+$lang->store->appBasicInfo      = '基本信息';
 $lang->store->screenshots      = '应用截图';
 $lang->store->appDynamic       = '应用动态';
 $lang->store->noDynamicArticle = '暂无动态';
 $lang->store->noScreenshot     = '暂无截图';
 
 $lang->store->support          = '支持';
-$lang->store->gitUrl           = '应用源码地址';
-$lang->store->dockerfileUrl    = 'Dockerfile源码地址';
-$lang->store->forumUrl         = '论坛地址';
+$lang->store->gitUrl           = '应用源码';
+$lang->store->dockerfileUrl    = 'Dockerfile';
+$lang->store->forumUrl         = '论坛';
 
