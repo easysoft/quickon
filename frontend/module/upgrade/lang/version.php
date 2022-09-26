fe197b27aba038e6c1e975154644891918b2efbb
wangjianhua <wangjianhua@easycorp.ltd>
2022-09-26 14:26:47 +0800

    * Upgrade version to 1.6.0.

--- frontend/module/upgrade/lang/version.php
+++ frontend/module/upgrade/lang/version.php
@@ -7,3 +7,4 @@ $lang->upgrade->fromVersions['1_1_2'] = '1.1.2';
 $lang->upgrade->fromVersions['1_2_0'] = '1.2.0';
 $lang->upgrade->fromVersions['1_4_0'] = '1.4.0';
 $lang->upgrade->fromVersions['1_4_1'] = '1.4.1';
+$lang->upgrade->fromVersions['1_6_0'] = '1.6.0';
