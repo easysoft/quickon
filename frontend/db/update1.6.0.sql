e511f898d6f00011eb593bc6799e80fcaf8c5fb3
wangjianhua <wangjianhua@easycorp.ltd>
2022-09-27 12:05:09 +0800

    * Modify upgrade SQL.

--- frontend/db/update1.6.0.sql
+++ frontend/db/update1.6.0.sql
@@ -1,2 +1,2 @@
 ALTER TABLE `q_instance` ADD COLUMN `ldapSettings` text AFTER `domain`;
-ALTER TABLE `q_instance` ADD COLUMN `ldapSnippetName` char(30) NOT NULL AFTER `domain`;
+ALTER TABLE `q_instance` ADD COLUMN `ldapSnippetName` char(30) NULL AFTER `domain`;
