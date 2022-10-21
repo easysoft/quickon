e90d2f7172a76c64ed187093e125f5c2027423fc
wangjianhua <wangjianhua@easycorp.ltd>
2022-10-21 12:19:37 +0800

    - Remove not used file.

--- frontend/module/system/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-$config->system->installldap = new stdclass;
-$config->system->installldap->requiredFields = "source,extra[host],extra[port],extra[bindDN],extra[bindPass],extra[baseDN]";
-
-$config->system->editldap = new stdclass;
-$config->system->editldap->requiredFields = "source,extra[host],extra[port],extra[bindDN],extra[bindPass],extra[baseDN]";
-
