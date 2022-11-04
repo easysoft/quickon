3d6d036ab0a99023741a820517975ff5d5546d20
wangjianhua <wangjianhua@easycorp.ltd>
2022-11-04 14:55:25 +0800

    + Add black list of domain.

--- frontend/module/instance/config.php
+++ frontend/module/instance/config.php
@@ -3,6 +3,8 @@ $config->instance = new stdclass;
 $config->instance->keepDomainList = array();
 $config->instance->keepDomainList['console'] = 'console';
 $config->instance->keepDomainList['demo']    = 'demo';
+$config->instance->keepDomainList['s3']      = 's3';
+$config->instance->keepDomainList['s3-api']  = 's3-api';
 
 $config->instance->seniorChartList = array();
 $config->instance->seniorChartList['zentao']     = ['zentao-biz', 'zentao-max'];
