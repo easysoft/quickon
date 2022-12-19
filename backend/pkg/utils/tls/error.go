9821129e7a6c2e6daffa6bdd64f1187c2b70915b
qishiyao <qishiyao@easycorp.ltd>
2022-12-19 11:29:11 +0800

    + Add new tls error.

--- backend/pkg/utils/tls/error.go
+++ backend/pkg/utils/tls/error.go
@@ -6,4 +6,6 @@ var (
 	ErrUnmatchedCertificate       = errors.New("certificate chain not matched")
 	ErrExpiredCertificate         = errors.New("certificate is expired")
 	ErrIncompleteCertificateChain = errors.New("certificate chain is uncompleted")
+	ErrParseCertificate           = errors.New("parse certificate failed")
+	ErrParsePrivateKey            = errors.New("parse private key failed")
 )
