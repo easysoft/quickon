3f16e6fc82a5f25c6b1ac0bae0d0f6d51ca321cf
zenggang <zenggang@easycorp.ltd>
2023-02-17 13:50:50 +0800

    * Fix bug#32058

--- frontend/db/update2.6.1.sql
+++ frontend/db/update2.6.1.sql
@@ -1,1 +1,2 @@
 ALTER TABLE `q_instance` ADD COLUMN `autoRestore` tinyint NULL DEFAULT 0 AFTER `backupKeepDays`;
+ALTER TABLE `q_solution` ADD `updatedDate` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP AFTER `createdAt`;
