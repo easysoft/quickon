d82d3ee5e0440d37dd74fef5b30ccc6eaade28b2
zenggang <zenggang@easycorp.ltd>
2023-02-22 15:58:12 +0800

    * Update version

--- frontend/db/update2.6.1.sql
+++ frontend/db/update2.6.1.sql
@@ -1,2 +1,1 @@
 ALTER TABLE `q_instance` ADD COLUMN `autoRestore` tinyint NULL DEFAULT 0 AFTER `backupKeepDays`;
-ALTER TABLE `q_solution` ADD `updatedDate` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP AFTER `createdAt`;
